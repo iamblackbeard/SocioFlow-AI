c6909d653c84d0c815b4025d46452d6f75f4b7c0
Vaibhavg987 <78644114+Vaibhavg987@users.noreply.github.com>
2025-01-04 19:47:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect, useRef } from "react";
+import { LangflowClient } from "./langflow/langflow";
+
+const LangflowComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [response, setResponse] = useState(null);
+  const [isStreaming, setIsStreaming] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const responseRef = useRef(null);
+
+  const handleSubmit = async () => {
+    const langflowClient = new LangflowClient(
+      "/api",
+      "AstraCS:cDWFtegIZKOImAgNttuFoJDK:dcdb7208f93bbd617a61c3f77770998b80ca171a29188a3d30fe20a6fdd905fc"
+    );
+
+    const tweaks = {
+      // Your tweaks configuration here
+    };
+
+    try {
+      const stream = false;
+      setIsStreaming(stream);
+      setIsLoading(true);
+      const response = await langflowClient.runFlow(
+        "5fb89765-f392-4542-9b28-9c81bd906c43", // Flow ID
+        "d6ab1cff-4a30-44b6-b143-5bbd6077642f", // Langflow ID
+        inputValue,
+        "chat",
+        "chat",
+        tweaks,
+        stream,
+        (data) => console.log("Stream Update:", data), // onUpdate
+        () => console.log("Stream Closed"), // onClose
+        (error) => console.error("Stream Error:", error) // onError
+      );
+
+      if (!stream && response) {
+        setResponse(response.outputs[0].outputs[0].artifacts.message);
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (response) {
+      responseRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [response]);
+  return (
+    <div className="container">
+      <h1>SocioFlow-AI: Social Media Analytics Tool</h1>
+      <p>
+        This is to analyze engagement data from mock social media accounts,
+        leveraging LangFlow and DataStax Astra DB. The module will provide
+        actionable insights into the performance of different types of social
+        media posts (e.g., carousels, reels, and static images).
+      </p>
+
+      {isLoading ? (
+        <p className="loading">Fetching response...</p>
+      ) : (
+        response && (
+          <p className="response" ref={responseRef}>
+            Response: <br /> {response}
+          </p>
+        )
+      )}
+      {error && <p className="error">{error}</p>}
+      <div className="input_fields">
+        <textarea
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter your message here"
+          rows="2"
+        />
+        <button onClick={handleSubmit} disabled={isStreaming}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LangflowComponent;
